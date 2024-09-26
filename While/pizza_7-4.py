b69a96d7b9610243758711905f2301ecfa327aca
Mahfouz Mohamed <figo66727@gamil.com>
2024-09-26 17:11:36 +0300

    Exericise commit

---
+++ While/pizza_7-4.py
@@ -0,0 +1,12 @@
+request = "Please tell us what toppings you want "
+
+while True:
+    topping = input(request )
+
+    if topping == 'quit':
+        break
+    else:
+        print("We will add" ,topping ,"to your pizza" )
+    
+
+
