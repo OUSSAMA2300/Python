e48def37a37bd5f7f9718cba3250efdd0dd061ae
Mahfouz Mohamed <figo66727@gamil.com>
2024-09-26 16:55:45 +0300

    second commit

--- While/counting.py
+++ While/counting.py
@@ -2,6 +2,8 @@ current_number = 0
 while current_number < 10:
     current_number += 1
     if current_number % 2 == 0:
-        continue  # Skip to just copy the odd numbers
+        continue  # Igonre the rest to just copy the odd numbers and 
+                  # and return to the beginning
 
     print(current_number)
+
