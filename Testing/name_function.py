1587474373a2d21bb0890b0033284bae16e8c7e8
Mahfouz Mohamed <figo66727@gamil.com>
2024-10-15 22:33:57 +0300

    Modification on args in name_function.py

--- Testing/name_function.py
+++ Testing/name_function.py
@@ -1,4 +1,4 @@
-def get_foramtted_name(first, last):
+def get_foramtted_name(first, middle, last):
     """Generated a neatly formatted name"""
-    full_name = first + ' ' + last
+    full_name = first + ' ' + middle + ' ' + last
     return full_name.title()
