0700e7f6f6ad6f503edb0cc4ae8ffa042c52702c
Mahfouz Mohamed <figo66727@gamil.com>
2024-10-16 21:25:45 +0300

    population added to the city_functions

--- Testing/city_country.py
+++ Testing/city_country.py
@@ -1,4 +1,7 @@
-def city_functions(city, country):
-    """printing a city and country in formatted way"""
-    city_country = city + ', ' + country   
+def city_functions(city, country, population=''):
+    """Describe a city"""
+    if population:
+        city_country = f"{city}, {country} -Population {population}"
+    else:
+        city_country = f"{city}, {country}"
     return city_country.title()
