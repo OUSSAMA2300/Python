03727ce19c638511cef8d1b6f4b3fd42905677fe
Mahfouz Mohamed <figo66727@gamil.com>
2024-10-16 21:24:52 +0300

    Nothing added to test_name_function.py

--- Testing/test_name_function.py
+++ Testing/test_name_function.py
@@ -10,8 +10,8 @@ class NamesTestCase(unittest.TestCase):
     
     def test_first_last_middle_name(self):
         """Do names like 'Wolfgang Amadeus Mozart' work?"""
-        formatted_name = get_foramtted_name(
+        name = get_foramtted_name(
             'wolfgang', 'mozart', 'amadeus')
-        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+        self.assertEqual(name, 'Wolfgang Amadeus Mozart')
 
 unittest.main()
