626b3020846ef0c601a87de51dfc20c310e8aadc
Mahfouz Mohamed <figo66727@gamil.com>
2024-10-18 22:46:48 +0300

    Updated ship module

--- Projects/Alien_Invasion/ship.py
+++ Projects/Alien_Invasion/ship.py
@@ -11,8 +11,8 @@ class Ship():
         self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of the screen
-        self.rect.centerx = self.screen_rect.bottom
-        self.rect.bottom = self.screen_rect.botoom
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
     def blitme(self):
         """Draw the ship at its current location"""
