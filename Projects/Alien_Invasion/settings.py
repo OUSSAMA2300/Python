9cd78c88925c799f844becf24996e5a884d04ec3
Mahfouz Mohamed <figo66727@gamil.com>
2024-10-22 18:15:41 +0300

    Added speed factort setting

--- Projects/Alien_Invasion/settings.py
+++ Projects/Alien_Invasion/settings.py
@@ -4,7 +4,9 @@ class Settings():
     def __init__(self):
         """Initialize the game's settings"""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1280
+        self.screen_height = 720
         self.bg_color = (230, 230, 230)
-         
+        
+        # Ship settings
+        self.ship_speed_factor = 1.5
