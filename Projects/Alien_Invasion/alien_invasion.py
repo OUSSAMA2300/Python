015314e3437be3fb6ccec1c0c86c26a03b2ec144
Mahfouz Mohamed <figo66727@gamil.com>
2024-10-25 18:11:55 +0300

    Adding group of bullets to the main func

--- Projects/Alien_Invasion/alien_invasion.py
+++ Projects/Alien_Invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+
 
 def run_game():
     # Initialize game and creat a screen object
@@ -15,11 +17,15 @@ def run_game():
     # Make a ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store the bullets in
+    bullets = Group()
+
     # Start the main loop for the game
     while True:
 
-        gf.check_events(ship)       
+        gf.check_events(ai_settings, screen, ship, bullets)       
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
