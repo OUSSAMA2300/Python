10c983c557573296b5e64b37a626c3b813e22397
Mahfouz Mohamed <figo66727@gamil.com>
2024-11-08 21:40:46 +0200

    deleted files

--- Exercises_in_Projects/settings.py
+++
@@ -1,9 +0,0 @@
-class Settings():
-
-    def __init__(self):
-
-        self.screen_width = 1080
-        self.screen_height = 720
-        self.bg_color = (255, 255, 255)
-
-        self.speed = 3.0
