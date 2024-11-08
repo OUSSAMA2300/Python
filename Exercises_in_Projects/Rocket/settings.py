80bcbda9b8138ca58b75dd970cba1fe8d6dae237
Mahfouz Mohamed <figo66727@gamil.com>
2024-11-08 21:39:37 +0200

    update

---
+++ Exercises_in_Projects/Rocket/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+
+    def __init__(self):
+
+        self.screen_width = 1080
+        self.screen_height = 720
+        self.bg_color = (255, 255, 255)
+
+        self.speed = 3.0
